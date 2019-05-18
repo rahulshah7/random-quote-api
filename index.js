51d259fdd208ddc036465ccb9b9c3d64473504bc
Rahul Shah <rahulshah7@outlook.com>
2019-05-19 00:33:27 +0100

    Enable CORS

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const quotes = require("./quotes.json");
 
-app.get("/api/random-quote", (req, res) => {
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/api/random-quote", (req, res, next) => {
   const quoteIndex = Math.floor(Math.random() * quotes.length);
   res.send(quotes[quoteIndex]);
 });
